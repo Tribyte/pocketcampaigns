2fa153cb8ff6db48bd6e4e3f22b5dbc388706321
herbglitch <heber.brau@gmail.com>
2020-10-22 22:28:33 -0600

    cards build

---
+++ frontend/build/precache-manifest.d8eaec8ec87f17b220a33008d0c0f51a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbfa545a6589bdfd17d4eb801f2036d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a43488013d4d85d7b55d",
+    "url": "/static/css/main.07d9c584.chunk.css"
+  },
+  {
+    "revision": "dd2a557aabb8f5ed9b3d",
+    "url": "/static/js/2.9f1cfdbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f1cfdbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a43488013d4d85d7b55d",
+    "url": "/static/js/main.b6b68f5d.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "fc1c69ca2d40bb995f17309e1ba8c4be",
+    "url": "/static/media/SVG-Campaigns.fc1c69ca.svg"
+  },
+  {
+    "revision": "069e0046d81fdca2aa10fcb539531f73",
+    "url": "/static/media/apps-ico.069e0046.svg"
+  },
+  {
+    "revision": "122306a40650d19ae6204501a4aeb78b",
+    "url": "/static/media/create-icon.122306a4.svg"
+  },
+  {
+    "revision": "31a3df009eb56ef4e02c02b18dbc2910",
+    "url": "/static/media/discord-icon.31a3df00.svg"
+  },
+  {
+    "revision": "eb883cc5995ba3b9cc74bee2186f818a",
+    "url": "/static/media/eye-ico.eb883cc5.svg"
+  },
+  {
+    "revision": "ba76b4cdf2952e973413683af2596676",
+    "url": "/static/media/file-ico.ba76b4cd.svg"
+  },
+  {
+    "revision": "f9ee9fde5ae8e55a865996447ee95f46",
+    "url": "/static/media/lock-ico.f9ee9fde.svg"
+  },
+  {
+    "revision": "fc1c69ca2d40bb995f17309e1ba8c4be",
+    "url": "/static/media/logo-ico.fc1c69ca.svg"
+  },
+  {
+    "revision": "cf72283728f44170d44f5e4c95a162f2",
+    "url": "/static/media/logout-ico.cf722837.svg"
+  },
+  {
+    "revision": "bcc8317caaca7cacfddb0e1996c43115",
+    "url": "/static/media/organize-icon.bcc8317c.svg"
+  },
+  {
+    "revision": "0140958f0922548972979516848cfc12",
+    "url": "/static/media/person-ico.0140958f.svg"
+  },
+  {
+    "revision": "bdde16c6fe25d533576a98eba60a9f79",
+    "url": "/static/media/plus-ico.bdde16c6.svg"
+  },
+  {
+    "revision": "3cd8358d503ce34552077bddab1a680c",
+    "url": "/static/media/search-ico.3cd8358d.svg"
+  },
+  {
+    "revision": "d9bca98cc8e64975da2dd481a7926ae5",
+    "url": "/static/media/settings-ico.d9bca98c.svg"
+  },
+  {
+    "revision": "1ff3803c29ef0fe959473c6bbd106d77",
+    "url": "/static/media/sick-play-icon.1ff3803c.svg"
+  },
+  {
+    "revision": "3d2ede1335403f5098118a54fd87e759",
+    "url": "/static/media/x-ico.3d2ede13.svg"
+  }
+]);
